e689409f8e268068c7df25f10b5f1f398e6a4a30
Constantin <clc.aarhusit@gmail.com>
2019-12-26 01:04:52 +0100

    new function + expanded the data collection model

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    task_name: {
-        type: String
-    }
+  task_name: {
+    type: String
+  },
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  isCompleted: {
+    type: Boolean
+  }
 });
 module.exports = User = mongoose.model('tasks', TaskSchema);
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,98 +1,145 @@
-'use strict'
+'use strict';
 
 const mongoose = require('mongoose');
-const Task = require('../models/Task')
+const Task = require('../models/Task');
 
 exports.plugin = {
-    register: (server, options) => {
-        server.route({
-            method: 'GET',
-            path: '/tasks',
-            handler: (req, h) => {
-                return Task.find((err, res) => {
-                    if (err) {
-                        return err
-                    }
-                    return res;
-                });
-            }
+  register: (server, options) => {
+    server.route({
+      method: 'GET',
+      path: '/tasks',
+      handler: (req, h) => {
+        return Task.find({ isCompleted: false }, (err, res) => {
+          if (err) {
+            return err;
+          }
+          return res;
         });
-        server.route({
-            method: 'GET',
-            path: '/task/{id}',
-            handler: (req, h) => {
-                return Task.FindOne({
-                        _id: mongoose.Types.ObjectId(req.params.id)
-                    },
-                    (err, doc) => {
-                        if (err) {
-                            return err, 'Internal MongoDB error'
-                        }
-                        if (!doc) {
-                            return 'Not Found';
-                        }
-                        return doc;
-                    });
-            }
+      }
+    });
+    server.route({
+      method: 'GET',
+      path: '/tasksHistory',
+      handler: (req, h) => {
+        return Task.find({ isCompleted: true }, (err, res) => {
+          if (err) {
+            return err;
+          }
+          return res;
         });
-        server.route({
-            method: 'POST',
-            path: '/task',
-            handler: (req, h) => {
-                var task = new Task()
-                task.task_name = req.payload.task_name;
-                return task.save().then((err, res) => {
-                    if (err) {
-                        return err;
-                    }
-                    return res;
-                });
+      }
+    });
+    server.route({
+      method: 'GET',
+      path: '/task/{id}',
+      handler: (req, h) => {
+        return Task.FindOne(
+          {
+            _id: mongoose.Types.ObjectId(req.params.id)
+          },
+          (err, doc) => {
+            if (err) {
+              return err, 'Internal MongoDB error';
             }
-        });
-        server.route({
-            method: 'PUT',
-            path: '/task/{id}',
-            handler: (req, h) => {
-                return Task.findOneAndUpdate({
-                        _id: req.params.id
-                    }, {
-                        task_name: req.payload.task_name
-                    },
-                    (err, result) => {
-                        if (err) {
-                            return err, 'Internal Mongodb Error';
-                        }
-                        if (!result) {
-                            return 'Not Found';
-                        }
-                        // if (result.n === 0) {
-                        //     return 'Not Found';
-                        // }
-                        return 204;
-                    });
+            if (!doc) {
+              return 'Not Found';
             }
+            return doc;
+          }
+        );
+      }
+    });
+    server.route({
+      method: 'POST',
+      path: '/task',
+      handler: (req, h) => {
+        var task = new Task();
+        task.task_name = req.payload.task_name;
+        task.isCompleted = false;
+        task.createdAt = Date.now();
+        task.updatedAt = Date.now();
+        return task.save().then((err, res) => {
+          if (err) {
+            return err;
+          }
+          return res;
         });
-        server.route({
-            method: 'DELETE',
-            path: '/task/{id}',
-            handler: (req, h) => {
-
-                return Task.deleteOne({
-                    _id: req.params.id
-
-                }, (err, res) => {
-                    if (err) {
-                        return err, 'Internal Mongodb Error';
-                    }
-                    if (res.n === 0) {
-                        return 'Not Found';
-                    }
-                    return 204;
-                });
-
+      }
+    });
+    server.route({
+      method: 'PUT',
+      path: '/task/{id}/complete',
+      handler: (req, h) => {
+        return Task.findOneAndUpdate(
+          {
+            _id: req.params.id
+          },
+          {
+            updatedAt: Date.now(),
+            isCompleted: true
+          },
+          (err, result) => {
+            if (err) {
+              return err, 'Internal Mongodb Error';
             }
-        });
-
-    },
-    name: 'api'
+            if (!result) {
+              return 'Not Found';
+            }
+            // if (result.n === 0) {
+            //     return 'Not Found';
+            // }
+            return 204;
+          }
+        );
+      }
+    });
+    server.route({
+      method: 'PUT',
+      path: '/task/{id}',
+      handler: (req, h) => {
+        return Task.findOneAndUpdate(
+          {
+            _id: req.params.id
+          },
+          {
+            task_name: req.payload.task_name,
+            updatedAt: Date.now()
+          },
+          (err, result) => {
+            if (err) {
+              return err, 'Internal Mongodb Error';
+            }
+            if (!result) {
+              return 'Not Found';
+            }
+            // if (result.n === 0) {
+            //     return 'Not Found';
+            // }
+            return 204;
+          }
+        );
+      }
+    });
+    server.route({
+      method: 'DELETE',
+      path: '/task/{id}',
+      handler: (req, h) => {
+        return Task.deleteOne(
+          {
+            _id: req.params.id
+          },
+          (err, res) => {
+            if (err) {
+              return err, 'Internal Mongodb Error';
+            }
+            if (res.n === 0) {
+              return 'Not Found';
+            }
+            return 204;
+          }
+        );
+      }
+    });
+  },
+  name: 'api'
 };
